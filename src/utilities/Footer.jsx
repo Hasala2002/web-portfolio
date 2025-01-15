44f05644b7945d46f9fb9ac312ffe638228b5d22
Hasala2002 <46789216+Hasala2002@users.noreply.github.com>
2025-01-14 18:31:24 -0600

    Update LinkedIn URL

--- src/utilities/Footer.jsx
+++ src/utilities/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
             <a href="https://github.com/Hasala2002" target="_blank"><SiGithub /></a>
             <a href="https://www.instagram.com/hasala2002/" target="_blank"><SiInstagram /></a>
             <a href="https://behance.net/gallery/135267581/Portfolio" target="_blank"><SiBehance /></a>
-            <a href="https://linkedin.com/in/hasala-heiyantuduwa-748639187/" target="_blank"><SiLinkedin /></a>
+            <a href="https://linkedin.com/in/hasala-heiyantuduwa/" target="_blank"><SiLinkedin /></a>
        </div>
        <div className="disclaimer with-tooltip" data-tooltip-content="Well, Obviously...">Made by me. ٩(˘◡˘)۶</div>
     </footer>
